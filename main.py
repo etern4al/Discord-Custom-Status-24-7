37d5f6eefa201d805b59bbf4aa1e16b9df2d0348
ven1ty <unripened@skiff.com>
2024-06-23 01:58:10 +0200

    Uncomment the Emoji part.

--- main.py
+++ main.py
@@ -58,11 +58,11 @@ def onliner(token, status):
                     "name": "Custom Status",
                     "id": "custom",
                     #Uncomment the below lines if you want an emoji in the status
-                    #"emoji": {
-                        #"name": "emoji name",
-                        #"id": "emoji id",
-                        #"animated": False,
-                    #},
+                    "emoji": {
+                        "name": "emoji name",
+                        "id": "emoji id",
+                        "animated": False,
+                    },
                 }
             ],
             "status": status,
